47529389b38237405ee06f34c65753acd93ad11b
sabam23 <sabamchedlishvili17@gmail.com>
2023-03-31 11:59:47 +0400

    fix nav-bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  logged: boolean = this.authService.isLoggedIn;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
